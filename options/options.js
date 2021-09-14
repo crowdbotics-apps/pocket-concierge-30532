54c1cca7ae6bad3cde353edce930b996847ced56
Crowdbotics <team@crowdbotics.com>
2021-09-14 13:39:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "pocket_concierge_30532Identifier",
+  url: "https://pocket_concierge_30532Identifier.botics.co",
+  api: "https://pocket_concierge_30532Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
